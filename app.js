c20ac2f1552c686eb91aaa6f2eaf3a89c397aa16
Dmytro Yanushkevych <onswarm@gmail.com>
2018-06-21 07:38:32 +0300

    get and post requests

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 // set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function(){
+app.get('/users', (req, res) => {
+  let users = [
+    {
+      first_name: 'John',
+      last_name: 'Doe',
+      age: 34,
+      gender: 'male'
+    },
+    {
+      first_name: 'Tom',
+      last_name: 'Jackson',
+      age: 23,
+      gender: 'male'
+    },
+    {
+      first_name: 'Tracy',
+      last_name: 'Smith',
+      age: 38,
+      gender: 'female'
+    },
+  ];
+
+  res.json(users);
+});
+
+app.get('/download', (req, res) => {
+  res.download(path.join(__dirname, '/downloads/preview_system.pdf'));
+} );
+
+app.get('/about', (req, res) => {
+  res.redirect('/about.html');
+} );
+
+app.post('/subscribe', (req, res) => {
+  let name = req.body.name;
+  let email =  req.body.email;
+
+  console.log(name+' has subscribed with '+email);
+});
+
+app.listen(3000, () => {
   console.log('Server started on port 3000...');
 });
